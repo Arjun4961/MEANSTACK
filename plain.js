eeb936ceef470a451fda87d000c3d7d652555be4
ARJUN <110219804+Arjun4961@users.noreply.github.com>
2022-10-21 12:04:08 +0530

    Delete plain.js

--- plain.js
+++
@@ -1,9 +0,0 @@
-var http=require('http');
-http.createServer(function(req,res){
-	res.writeHead(200,{'content-type':'text/html'});
-	res.write("Hello world");
-	res.write("<br>");
-	res.write("<h1> hi hello</h1>");
-	res.end();
-	}).listen(2020);
-	console.log("hello");
