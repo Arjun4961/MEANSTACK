c1f12a1b2d5fa402ce66442eec1818f6a3066a86
ARJUN <110219804+Arjun4961@users.noreply.github.com>
2022-11-11 11:37:36 +0530

    Delete sample.js

--- MONGODB/sample.js
+++
@@ -1,9 +0,0 @@
-const express=require('express');
-const app=express();
-const mongoose=require('mongoose');
-mongoose.connect("mongodb://127.0.0.1:27017/CSE_C",(err)=>{
-
-if(err)
-console.log("DB NOT connected-Error");
-else
-console.log("DB Connected");});
